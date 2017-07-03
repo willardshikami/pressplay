a83b98ff51cc41f2d09817e72429871e87d32460
willardshikami <willardshikami@gmail.com>
2017-07-03 12:13:30 +0300

    created models, db schema and returned data as API

--- models/playlists.js
+++ models/playlists.js
@@ -2,5 +2,32 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 //the schema
-const playlistSchema = mongoose.Schema;
-  
+const playlistSchema = mongoose.Schema({
+	playlist_name:{
+		type: String,
+		required: true
+	},
+	username:{
+		type: String,
+		required: true
+	}, 
+	number_of_songs: {
+		type: String,
+		required: true
+	},
+	image_url:{
+		type: String
+	},
+	added_date: {
+		type: Date,
+		default: Date.now
+	}
+});
+
+//defining the model
+const Playlist = module.exports = mongoose.model('playlist', playlistSchema);
+
+//get resource
+module.exports.getPlaylists = function(callback, limit){
+	Playlist.find(callback).limit(limit);
+}
--- server.js
+++ server.js
@@ -6,8 +6,14 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+//playlist
+Playlist = require('./models/playlists');
+
 //setting the client side
 app.use(express.static('client'));
+
+//bodyparser middleware
+app.use(bodyParser.json());
 
 //port var
 const port = 5000;
@@ -15,6 +21,26 @@ const port = 5000;
 //home
 app.get('/', (req, res) => {
     res.send('Invalid endpoint')
+});
+
+//connecting to the DB
+mongoose.connection.openUri('mongodb://localhost/music')
+const db = mongoose.connection;
+
+//checking for errors in DB connection
+db.on('error', console.error.bind(console, 'connection error: '));
+db.once('open', () => {
+    console.log('we are connected');
+});
+
+//get playlists
+app.get('/api/playlists', (req, res) => {
+    Playlist.getPlaylists((err, playlists) => {
+        if(err){
+            throw err;
+        }
+        res.json(playlists)
+    });
 });
 
 app.listen(port, () => {
